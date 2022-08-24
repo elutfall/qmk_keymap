9a08f3baf13ecb13462c1bd8fc7cda81744da460
Elias Lutfallah <dontyoumonkeywiththemonkey@gmail.com>
2022-08-24 15:36:49 -0500

    Update config.h

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
